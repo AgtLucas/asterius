6d38afb35f7940f0a727b3b39616dd3b956da155
Shao Cheng <cheng.shao@tweag.io>
2018-07-17 20:14:38 +0800

    no message

--- asterius/test/array/array.hs
+++ asterius/test/array/array.hs
@@ -1,8 +1,19 @@
-import Data.Array
+import Data.Array.IArray
+import Data.Array.Unboxed
 
 foreign import javascript "console.log(${1})" js_print_int :: Int -> IO ()
 
+--foreign import ccall unsafe "js_print_int" js_print_int :: Int -> IO ()
+facts :: [Int]
+facts = scanl (*) 1 [1 ..]
+
+boxedArr :: Array Int Int
+boxedArr = listArray (0, 5) facts
+
+unboxedArr :: UArray Int Int
+unboxedArr = listArray (0, 5) facts
+
 main :: IO ()
 main = do
-  let arr = listArray (0, 3) [2, 3, 5, 7]
-  js_print_int $ arr ! (3 :: Int)
+  js_print_int $ boxedArr ! 5
+  js_print_int $ unboxedArr ! 5
